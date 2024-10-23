d42668151c2048a0de6c9d2320b0b1baab7470b3
João Vitor <64005648+JaoVitorMS@users.noreply.github.com>
2024-10-23 22:35:55 +0000

    first commit

---
+++ questao1.java
@@ -0,0 +1,17 @@
+public class questao1{
+    public static void main(String[] args){
+
+        int k, soma, indice;
+        k = 0;
+        soma = 0;
+        indice = 13;
+        while(k <= indice){
+            k = k+1;
+            soma = soma + k;
+        }
+        System.out.println(soma);
+    }
+/*Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
+Imprimir(SOMA); */
+    
+}
